cda67930b4119a17c33da6b9d52028d970f88871
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-11-07 13:35:46 +0700

    Feature: Config auditor for jpa

---
+++ src/main/java/com/tsoftware/qtd/entity/AbstractAuditEntity.java
@@ -0,0 +1,32 @@
+package com.tsoftware.qtd.entity;
+
+
+import com.tsoftware.qtd.configuration.CustomAuditingEntityListener;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.ZonedDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(CustomAuditingEntityListener.class)
+public class AbstractAuditEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@CreatedDate
+	private ZonedDateTime createdAt;
+	@LastModifiedDate
+	private ZonedDateTime updatedAt;
+	@LastModifiedBy
+	private String lastModifiedBy;
+	@CreatedBy
+	private String createdBy;
+}
