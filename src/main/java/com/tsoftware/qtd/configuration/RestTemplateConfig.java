15b53b52bf5de92fb9092421b287339ca24ed584
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-11-21 23:54:43 +0700

    Fix: Server.key

--- src/main/java/com/tsoftware/qtd/configuration/RestTemplateConfig.java
+++ src/main/java/com/tsoftware/qtd/configuration/RestTemplateConfig.java
@@ -8,7 +8,6 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
-  @Bean // chấp nhận tất cả các chứng chỉ ssl, bao gồm tự ký
+  //  @Bean // chấp nhận tất cả các chứng chỉ ssl, bao gồm tự ký
   public RestTemplate getRestTemplate()
       throws NoSuchAlgorithmException, KeyManagementException, NoSuchAlgorithmException {
 
