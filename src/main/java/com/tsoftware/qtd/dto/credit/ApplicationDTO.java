f13b1891651579e8fe18f8748f580f9467a016a3
HuyNguyen <huy.nguyen@itech.asia>
2024-12-11 23:47:29 +0700

    Feat: Add some codes

--- src/main/java/com/tsoftware/qtd/dto/credit/ApplicationDTO.java
+++ src/main/java/com/tsoftware/qtd/dto/credit/ApplicationDTO.java
@@ -1,24 +1,27 @@
 package com.tsoftware.qtd.dto.credit;
 
-import com.tsoftware.qtd.constants.EnumType.LoanSecurityType;
-import com.tsoftware.qtd.constants.EnumType.LoanStatus;
+import com.tsoftware.qtd.constants.EnumType.ApplicationStep;
+import com.tsoftware.qtd.dto.Transaction;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
-import lombok.Builder;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
-public class CreditRequest {
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationDTO {
 
   private BigDecimal amount;
+  private ApplicationStep step;
+  List<Transaction> transactions;
   private ZonedDateTime startDate;
   private ZonedDateTime dueDate;
   private BigDecimal interestRate;
   private BigDecimal amountPaid;
   private BigDecimal currentOutstandingDebt;
-  private LoanStatus status;
-  private LoanSecurityType loanSecurityType;
 }
