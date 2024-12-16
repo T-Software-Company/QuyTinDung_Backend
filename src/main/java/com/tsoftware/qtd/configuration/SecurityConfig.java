bc2f65506dd3b3757c5b91d802eb07300de27ba2
HuyNguyen <huy.nguyen@itech.asia>
2024-12-16 20:15:40 +0700

    Feat: Revert code

--- src/main/java/com/tsoftware/qtd/configuration/SecurityConfig.java
+++ src/main/java/com/tsoftware/qtd/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.tsoftware.qtd.configuration;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,8 +24,23 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-        .csrf(AbstractHttpConfigurer::disable);
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/api-docs", "/api-docs/**", "/swagger-ui/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        //        																		.requestMatchers(protectedClientPaths)
+        //        																		.authenticated()
+        //        																		.anyRequest()
+        //        																		.hasRole(Role.ADMIN.name()))
+        .oauth2ResourceServer(
+            oauth2 ->
+                oauth2.jwt(
+                    jwtConfigurer ->
+                        jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults());
     return httpSecurity.build();
   }
 
